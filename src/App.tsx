aa20e8c0a7a912b7aa8ef85e74b1fed949636688
Jorge-Rmz <jorgre5675420j@gmail.com>
2025-02-03 18:31:16 -0600

    formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-
+import styles from "./App.module.css"
+import Form from "./componets/Form/Form"
 
 function App() {
 
   return (
     <>
-     <h1>buscador de clima</h1>
-    </>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+    <div className={styles.container}>
+      <Form/>
+      <p>2</p>
+    </div>
+
+      </>
   )
 }
 
