a3f20614cf73caa90cc543b37e9996ff43c29b72
Jorge-Rmz <jorgre5675420j@gmail.com>
2025-02-15 20:53:57 -0600

    consultado el clima

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import styles from "./App.module.css"
 import Form from "./componets/Form/Form"
+import WeatherDetail from "./componets/WeatherDetail/WeatherDetail";
+import useWeather from "./hooks/useWeather";
 
 function App() {
-
+  const { weather, fetchWeather, hasWeatherData } = useWeather();
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
-    <div className={styles.container}>
-      <Form/>
-      <p>2</p>
-    </div>
+      <div className={styles.container}>
+        <Form fetchWeather={fetchWeather} />
+        {hasWeatherData &&
+          <WeatherDetail weather={weather} />
+        }
+      </div>
 
-      </>
+    </>
   )
 }
 
--- src/componets/Form/Form.tsx
+++ src/componets/Form/Form.tsx
@@ -2,13 +2,13 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import type { SearchType } from "../../interfaces";
 import { countries } from "../../data/countries";
 import styles from './Form.module.css'
-// import Alert from "../Alert/Alert";
+import Alert from "../Alert/Alert";
 
-// type FormProps = {
-//     fetchWeather: (search: SearchType) => Promise<void>
-// }
+type FormProps = {
+    fetchWeather: (search: SearchType) => Promise<void>  
+}
 
-export default function Form() {
+export default function Form({fetchWeather}: FormProps) {
 
     const [search, setSearch] = useState<SearchType>({
         city: '',
@@ -23,22 +23,22 @@ export default function Form() {
         })
     }
 
-    // const handleSubmit = ( e: FormEvent<HTMLFormElement>) => {
-    //     e.preventDefault()
-    //     if(Object.values(search).includes('')) {
-    //         setAlert('Todos los campos son obligatorios')
-    //         return
-    //     }
-    //     fetchWeather(search)
-    // }
+    const handleSubmit = ( e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if(Object.values(search).includes('')) {
+            setAlert('Todos los campos son obligatorios')
+            return
+        }   
+        fetchWeather(search);
+    }
 
     return (
         <form 
             className={styles.form}
-            // onSubmit={handleSubmit}
+            onSubmit={handleSubmit}
         >
 
-            {/* {alert && <Alert>{alert}</Alert>} */}
+            {alert && <Alert>{alert}</Alert>}
             <div className={styles.field}>
                 <label htmlFor="city">Ciudad:</label>
                 <input
